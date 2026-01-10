65ba450134dcfca96065bc31088cbb2f367152d0
vinnylimm <vinny.mvfl12@gmail.com>
2026-01-10 17:08:01 -0300

    first commit

---
+++ dscatalog_project/backend/src/main/java/com/devsuperior/dscatalog/resources/CategoryResource.java
@@ -0,0 +1,28 @@
+package com.devsuperior.dscatalog.resources;
+
+import java.util.List;
+import java.util.ArrayList;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import com.devsuperior.dscatalog.entities.Category;
+import com.devsuperior.dscatalog.services.CategoryService;
+
+@RestController
+@RequestMapping("/categories")
+public class CategoryResource {
+
+	@Autowired
+	private CategoryService service;
+
+	@GetMapping
+	public ResponseEntity<List<Category>> findAll(){
+		List<Category> list = service.findAll();
+		return ResponseEntity.ok().body(list);
+	}
+
+}
