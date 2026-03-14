602638278dc263dc0a024d50f79c08eba7dd0256
vinnylimm <vinny.mvfl12@gmail.com>
2026-03-14 17:27:52 -0300

    ProductRepositoryTests and ProductServicesTests

--- dscatalog_project/backend/src/main/java/com/devsuperior/dscatalog/services/ProductService.java
+++ dscatalog_project/backend/src/main/java/com/devsuperior/dscatalog/services/ProductService.java
@@ -11,6 +11,7 @@ import com.devsuperior.dscatalog.services.exceptions.ResourceNotFoundException;
 import jakarta.persistence.EntityNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
@@ -63,14 +64,14 @@ public class ProductService {
 
 	@Transactional(propagation = Propagation.SUPPORTS)
 	public void delete(Long id){
-		if(!repository.existsById(id)){
-			throw new ResourceNotFoundException("Resource not found");
-		}
 		try{
 			repository.deleteById(id);
 		}
+		catch (EmptyResultDataAccessException e){
+			throw new ResourceNotFoundException("Id not found " + id);
+		}
 		catch(DataIntegrityViolationException e){
-			throw new DatabaseException("Integrity fail");
+			throw new DatabaseException("Integrity violation");
 		}
 	}
 
---
+++ dscatalog_project/backend/src/test/java/com/devsuperior/dscatalog/repositories/ProductRepositoryTests.java
@@ -0,0 +1,50 @@
+package com.devsuperior.dscatalog.repositories;
+
+import com.devsuperior.dscatalog.entities.Product;
+import com.devsuperior.dscatalog.tests.Factory;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.data.jpa.test.autoconfigure.DataJpaTest;
+
+import java.util.Optional;
+
+@DataJpaTest
+public class ProductRepositoryTests {
+
+    @Autowired
+    private ProductRepository repository;
+
+    private long existingId;
+    private long countTotalProducts;
+
+    @BeforeEach
+    void setup() throws Exception {
+        existingId = 1L;
+        countTotalProducts = 25L;
+    }
+
+    @Test
+    void saveShouldPersistWithAutoincrementWhenIdIsNull() {
+        Product product = Factory.createProduct();
+        product.setId(null);
+
+        product = repository.save(product);
+
+        Assertions.assertNotNull(product.getId());
+        Assertions.assertEquals(countTotalProducts + 1, product.getId());
+    }
+
+    @Test
+    public void deleteShouldDeleteObjectWhenIdExists(){
+
+        long existingId = 1L;
+
+        repository.deleteById(existingId);
+
+        Optional<Product> result = repository.findById(existingId);
+        Assertions.assertFalse(result.isPresent());
+    }
+
+}
