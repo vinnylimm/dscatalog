194242fc1021bc76f94d901d4f4a428b156a75ee
vinnylimm <vinny.mvfl12@gmail.com>
2026-03-17 19:15:06 -0300

    test deleteShouldThrowDatabaseExceptionWhenDependentId

--- dscatalog_project/backend/src/test/java/com/devsuperior/dscatalog/services/ProductServiceTests.java
+++ dscatalog_project/backend/src/test/java/com/devsuperior/dscatalog/services/ProductServiceTests.java
@@ -1,11 +1,18 @@
 package com.devsuperior.dscatalog.services;
 
 import com.devsuperior.dscatalog.repositories.ProductRepository;
+import com.devsuperior.dscatalog.services.exceptions.DatabaseException;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.dao.DataIntegrityViolationException;
 
-@ExtendWith()
+@ExtendWith(MockitoExtension.class)
 public class ProductServiceTests {
 
     @InjectMocks
@@ -13,5 +20,34 @@ public class ProductServiceTests {
 
     @Mock
     private ProductRepository repository;
+
+    private long existingId;
+    private long nonExistingId;
+    private long dependentId;
+
+    @BeforeEach
+    void setUp() throws Exception {
+        existingId = 1L;
+        nonExistingId = 1000L;
+        dependentId = 4L;
+
+        Mockito.lenient().doNothing().when(repository).deleteById(existingId);
+        Mockito.lenient().doThrow(DataIntegrityViolationException.class).when(repository).deleteById(dependentId);
+    }
+
+    @Test
+    public void deleteShouldDoNothingWhenIdExists() {
+        Assertions.assertDoesNotThrow(() -> {
+            service.delete(existingId);
+        });
+        Mockito.verify(repository, Mockito.times(1)).deleteById(existingId);
+    }
+
+    @Test
+    public void deleteShouldThrowDatabaseExceptionWhenDependentId() {
+        Assertions.assertThrows(DatabaseException.class, () -> {
+            service.delete(dependentId);
+        });
+    }
 
 }
