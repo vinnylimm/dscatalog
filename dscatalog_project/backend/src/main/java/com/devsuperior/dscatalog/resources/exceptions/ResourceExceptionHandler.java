67e10fc1ba3ecf0ee548fbbb7b8d1f7cf9e96c98
vinnylimm <vinny.mvfl12@gmail.com>
2026-07-16 18:57:24 -0300

    Custom validation response

--- dscatalog_project/backend/src/main/java/com/devsuperior/dscatalog/resources/exceptions/ResourceExceptionHandler.java
+++ dscatalog_project/backend/src/main/java/com/devsuperior/dscatalog/resources/exceptions/ResourceExceptionHandler.java
@@ -4,6 +4,8 @@ import java.time.Instant;
 
 import jakarta.servlet.http.HttpServletRequest;
 
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.http.ResponseEntity;
 import org.springframework.http.HttpStatus;
@@ -36,6 +38,23 @@ public class ResourceExceptionHandler {
 		err.setError("Database integrity violation");
 		err.setMessage(e.getMessage());
 		err.setPath(request.getRequestURI());
+
+		return ResponseEntity.status(status).body(err);
+	}
+
+	@ExceptionHandler(MethodArgumentNotValidException.class)
+	public ResponseEntity<ValidationError> validation(MethodArgumentNotValidException e, HttpServletRequest request) {
+		HttpStatus status = HttpStatus.UNPROCESSABLE_CONTENT;
+		ValidationError err = new ValidationError();
+		err.setTimestamp(Instant.now());
+		err.setStatus(status.value());
+		err.setError("Validation exception");
+		err.setMessage(e.getMessage());
+		err.setPath(request.getRequestURI());
+
+		for (FieldError f :e.getBindingResult().getFieldErrors()){
+			err.addError(f.getField(), f.getDefaultMessage());
+		}
 
 		return ResponseEntity.status(status).body(err);
 	}
